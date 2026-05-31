13d28258c364557aa0be4f47b0a88f9b820560cd
KGF STUDIO <harsiddhpatel37@gmail.com>
2026-05-31 13:46:23 +0000

    j

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+
 import App from "./App";
 
-const rootElement = document.getElementById("root")!;
-const root = ReactDOM.createRoot(rootElement);
-
+const root = createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
